cc03e9519de53713ae9130ae85a5fe596f6a02fb
tuxbuesch <tuxbuesch@1c258df4-fefe-0310-a50c-9dade605666b>
2005-08-24 12:38:20 +0000

    clean up

--- trunk/.bcm430x.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/st3/bcm43xx/bcm430x.ko := ld -m elf_i386 -r -o /home/st3/bcm43xx/bcm430x.ko /home/st3/bcm43xx/bcm430x.o /home/st3/bcm43xx/bcm430x.mod.o
--- trunk/.tmp_versions/bcm430x.mod
+++
@@ -1,2 +0,0 @@
-/home/st3/bcm43xx/bcm430x.ko
-/home/st3/bcm43xx/bcm430x.o
