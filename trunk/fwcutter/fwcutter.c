3582096d4e6ae61be2d4b41010bd53e9b9fd867d
tuxbuesch <tuxbuesch@1c258df4-fefe-0310-a50c-9dade605666b>
2005-09-12 17:16:20 +0000

    better commandline args parsing.

--- trunk/fwcutter/fwcutter.c
+++ trunk/fwcutter/fwcutter.c
@@ -1,7 +1,8 @@
 /*
  * firmware cutter for broadcom 43xx wireless driver files
  * 
- * Copyright (c) 2005 Martin Langer <martin-langer@gmx.de>
+ * Copyright (c) 2005 Martin Langer <martin-langer@gmx.de>,
+ *               2005 Michael Buesch <mbuesch@freenet.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,8 +32,19 @@ typedef unsigned char byte;
 #define BYTE_ORDER_DDCCBBAA    0x01  /* 4 bytes swapped in source (DDCCBBAA instead of AABBCCDD) */
 #define INIT_VAL_08_MISSING    0x02  /* initval 8 is missing in older driver files */
 
+#define fwcutter_stringify_1(x)	#x
+#define fwcutter_stringify(x)	fwcutter_stringify_1(x)
+#define FWCUTTER_VERSION	fwcutter_stringify(FWCUTTER_VERSION_)
+
 #include "md5.h"
 #include "fwcutter_list.h"
+
+
+struct cmdline_args {
+	const char *infile;
+};
+
+static struct cmdline_args cmdargs;
 
 
 static void write_ddccbbaa(FILE *f, byte *buffer, int len) 
@@ -162,78 +174,159 @@ static void extract_iv(const char *infile, uint8_t flags, uint32_t pos)
 	free(filedata);
 }
 
+static void print_supported_files(void)
+{
+	int i;
+
+	printf("fwcutter supports these driver source files:\n");
+	for (i = 0; i < FILES; ++i) {
+		printf("%s\t %s \t md5: %s\n", files[i].name, 
+	 	       files[i].version, files[i].md5);
+	}
+}
+
+static void print_banner(void)
+{
+	printf("fwcutter " FWCUTTER_VERSION "\n");
+}
+
+static void print_usage(int argc, char *argv[])
+{
+	print_banner();
+	printf("\nUsage: %s [OPTION] [driver.sys]\n", argv[0]);
+	printf("  -l             list supported driver versions\n");
+	printf("  -v             print fwcutter version\n");
+	printf("  -h|--help      print this help\n");
+	printf("\nExample: %s bcmwl5.sys\n"
+	       "         to extract the firmware blobs from bcmwl5.sys\n", argv[0]);
+}
+
+static int cmp_arg(char *arg, const char *template, char **param)
+{
+	size_t arg_len, template_len;
+
+	if (param) {
+		arg_len = strlen(arg);
+		template_len = strlen(template);
+		/* maybe we have a merged parameter here. */
+		if (arg_len > template_len) {
+			if (memcmp(arg, template, template_len) == 0) {
+				*param = arg + template_len;
+				return 0;
+			}
+			return -1;
+		} else if (arg_len != template_len)
+			return -1;
+	}
+	if (strcmp(arg, template) == 0)
+		return 0;
+
+	return -1;
+}
+
+static int parse_args(int argc, char *argv[])
+{
+	int i;
+	char *arg;
+	char *next_arg;
+	char *param;
+
+	if (argc < 2)
+		goto out_usage;
+	for (i = 1; i < argc; i++) {
+		param = 0;
+		arg = argv[i];
+		if (i + 1 < argc)
+			next_arg = argv[i + 1];
+		else
+			next_arg = 0;
+		if (cmp_arg(arg, "-l", 0) == 0) {
+			print_supported_files();
+			return 1;
+		} else if (cmp_arg(arg, "-v", 0) == 0) {
+			print_banner();
+			return 1;
+		} else if (cmp_arg(arg, "-h", 0) == 0 ||
+			   cmp_arg(arg, "--help", 0) == 0) {
+			goto out_usage;
+		} else if (cmp_arg(arg, "-i", &param) == 0) {
+			if (param)
+				next_arg = param;
+			else
+				i++;
+			if (!next_arg) {
+				printf("-i needs a parameter\n");
+				return -1;
+			}
+			cmdargs.infile = next_arg;
+		} else {
+			cmdargs.infile = arg;
+			break;
+		}
+	}
+
+	if (!cmdargs.infile)
+		goto out_usage;
+	return 0;
+
+out_usage:
+	print_usage(argc, argv);
+	return -1;	
+}
+
 int main(int argc, char *argv[])
 {
 	unsigned char buffer[16384], signature[16];
 	char md5sig[33];
-	char *cp;
 	FILE *in = stdin;
-	int i, opt, count = 0;
+	int i, count = 0;
 	struct MD5Context md5c;
 
-	for (i = 1; i < argc; i++) {
-		cp = argv[i];
-		if (*cp == '-') {
-			if (strlen(cp) == 1) {
-				i++;
-				break;
-			}
-			opt = *(++cp);
-			if (isupper(opt)) {
-				opt = tolower(opt);
-			}
+	i = parse_args(argc, argv);
+	if (i == 1)
+		return 0;
+	else if (i != 0)
+		return i;
 
-			switch (opt) {
-			case 'i':
-				cp++;
-				if ((in = fopen(cp, "rb")) == NULL) {
-					fprintf(stderr, "Cannot open input file %s\n", cp);
-					return 2;
-				}
+	if ((in = fopen(cmdargs.infile, "rb")) == NULL) {
+		fprintf(stderr, "Cannot open input file %s\n", cmdargs.infile);
+		return 2;
+	}
 
-				MD5Init(&md5c);
-				while ((i = (int) fread(buffer, 1, sizeof buffer, in)) > 0) {
-					MD5Update(&md5c, buffer, (unsigned) i);
-				}
-				MD5Final(signature, &md5c);
-				
-				snprintf(md5sig, sizeof md5sig,
-					 "%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x",
-					 signature[0], signature[1], signature[2], signature[3],
-					 signature[4], signature[5], signature[6], signature[7],
-					 signature[8], signature[9], signature[10], signature[11],
-					 signature[12], signature[13], signature[14], signature[15]);
+	MD5Init(&md5c);
+	while ((i = (int) fread(buffer, 1, sizeof buffer, in)) > 0)
+		MD5Update(&md5c, buffer, (unsigned) i);
+	MD5Final(signature, &md5c);
 
-				for (i = 0; i < FILES; ++i) {
-					if (strcasecmp(md5sig, files[i].md5) == 0) {
-						printf("Your firmware file is known. It's version %s.\n", files[i].version);
-						extract_fw(cp, "bcm430x_microcode2.fw", files[i].flags, files[i].uc2_pos, files[i].uc2_length);
-						extract_fw(cp, "bcm430x_microcode4.fw", files[i].flags, files[i].uc4_pos, files[i].uc4_length);
-						extract_fw(cp, "bcm430x_microcode5.fw", files[i].flags, files[i].uc5_pos, files[i].uc5_length);
-						extract_fw(cp, "bcm430x_pcm4.fw", files[i].flags, files[i].pcm4_pos, files[i].pcm4_length);
-						extract_fw(cp, "bcm430x_pcm5.fw", files[i].flags, files[i].pcm5_pos, files[i].pcm5_length);
-						extract_iv(cp, files[i].flags, files[i].iv_pos);
-						++count;
-					}
-				}
+	snprintf(md5sig, sizeof md5sig,
+		 "%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x%.2x",
+		 signature[0], signature[1], signature[2], signature[3],
+		 signature[4], signature[5], signature[6], signature[7],
+		 signature[8], signature[9], signature[10], signature[11],
+		 signature[12], signature[13], signature[14], signature[15]);
 
-				if (count == 0) {
-					fputs("driver version unknown or wrong input file.\n", stderr);
-					return 1;
-				}
-
-				break;
-			case 'l':
-				printf("fwcutter supports these driver source files:\n");
-				for (i = 0; i < FILES; ++i)
-					printf("%s\t %s \t md5: %s\n", files[i].name, 
-					       files[i].version, files[i].md5);
-				break;
-			case 'v':
-				printf("fwcutter 0.0.1\n");
-				break;
-			}
+	for (i = 0; i < FILES; ++i) {
+		if (strcasecmp(md5sig, files[i].md5) == 0) {
+			printf("Your firmware file is known. It's version %s.\n", files[i].version);
+			extract_fw(cmdargs.infile, "bcm430x_microcode2.fw",
+				   files[i].flags, files[i].uc2_pos, files[i].uc2_length);
+			extract_fw(cmdargs.infile, "bcm430x_microcode4.fw",
+				   files[i].flags, files[i].uc4_pos, files[i].uc4_length);
+			extract_fw(cmdargs.infile, "bcm430x_microcode5.fw",
+				   files[i].flags, files[i].uc5_pos, files[i].uc5_length);
+			extract_fw(cmdargs.infile, "bcm430x_pcm4.fw",
+				   files[i].flags, files[i].pcm4_pos, files[i].pcm4_length);
+			extract_fw(cmdargs.infile, "bcm430x_pcm5.fw",
+				   files[i].flags, files[i].pcm5_pos, files[i].pcm5_length);
+			extract_iv(cmdargs.infile, files[i].flags, files[i].iv_pos);
+			++count;
+			break;
 		}
+	}
+
+	if (count == 0) {
+		fputs("driver version unknown or wrong input file.\n", stderr);
+		return 1;
 	}
 
 	return 0;
