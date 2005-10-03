e528567ac5bfa773e688dce3ba5a04c74ef91579
tuxbuesch <tuxbuesch@1c258df4-fefe-0310-a50c-9dade605666b>
2005-10-03 13:03:04 +0000

    add files for PIO code.

---
+++ trunk/driver/bcm430x_pio.c
@@ -0,0 +1,29 @@
+/*
+
+  Broadcom BCM430x wireless driver
+
+  PIO Transmission
+
+  Copyright (c) 2005 Michael Buesch <mbuesch@freenet.de>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; see the file COPYING.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Steet, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+
+*/
+
+#include "bcm430x.h"
+#include "bcm430x_pio.h"
+
+/* vim: set ts=8 sw=8 sts=8: */
---
+++ trunk/driver/bcm430x_pio.h
@@ -0,0 +1,6 @@
+#ifndef BCM430x_PIO_H_
+#define BCM430x_PIO_H_
+
+
+
+#endif /* BCM430x_PIO_H_ */
