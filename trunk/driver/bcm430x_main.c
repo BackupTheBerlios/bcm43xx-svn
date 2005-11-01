252817754d9789be5d14788ff2968cf52ce96df2
marlanx <marlanx@1c258df4-fefe-0310-a50c-9dade605666b>
2005-11-01 22:17:01 +0000

    TRENDnet TEW-401PC added

--- trunk/driver/bcm430x_main.c
+++ trunk/driver/bcm430x_main.c
@@ -132,6 +132,7 @@ static struct pci_device_id bcm430x_pci_tbl[] = {
 	{ PCI_VENDOR_ID_BROADCOM, 0x4320, 0x1057, 0x7025, 0, 0, 0 }, /* Motorola WN825G PC Card */
 	{ PCI_VENDOR_ID_BROADCOM, 0x4320, 0x106b, 0x004e, 0, 0, 0 }, /* Apple AirPort Extreme Card */
 	{ PCI_VENDOR_ID_BROADCOM, 0x4320, 0x14e4, 0x0013, 0, 0, 0 }, /* Linksys WMP54G PCI Card */
+	{ PCI_VENDOR_ID_BROADCOM, 0x4320, 0x14e4, 0x0417, 0, 0, 0 }, /* TRENDnet TEW-401PC */
 	{ PCI_VENDOR_ID_BROADCOM, 0x4320, 0x1737, 0x0015, 0, 0, 0 }, /* Linksys WMP54GS PC Card */
 	{ PCI_VENDOR_ID_BROADCOM, 0x4320, 0x1737, 0x4320, 0, 0, 0 }, /* Linksys WPC54G PC Card */
 	{ PCI_VENDOR_ID_BROADCOM, 0x4320, 0x1799, 0x7001, 0, 0, 0 }, /* Belkin F5D7001 PCI Card */
