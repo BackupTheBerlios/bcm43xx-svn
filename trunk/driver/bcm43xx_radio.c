4402265a2d5356aa8d941416b2018ab9f64c3ec4
st3 <st3@1c258df4-fefe-0310-a50c-9dade605666b>
2005-12-07 19:03:07 +0000

    fix radio_set_tx_iq

--- trunk/driver/bcm43xx_radio.c
+++ trunk/driver/bcm43xx_radio.c
@@ -1158,22 +1158,14 @@ void bcm43xx_radio_set_tx_iq(struct bcm43xx_private *bcm)
 	static const u8 data_high[5] = { 0x00, 0x40, 0x80, 0x90, 0xD0 };
 	static const u8 data_low[5]  = { 0x00, 0x01, 0x05, 0x06, 0x0A };
 	u16 tmp = bcm43xx_radio_read16(bcm, 0x001E);
-	s16 value = 0x00C0;
-	int i,j;
+	int i, j;
 	
-	for (i = 0; i < 5; i++) {
-		for (j = 0; j < 5; j++) {
-			if (tmp == (data_high[i] | data_low[j])) {
-				//XXX: Byteorder?
-				value |= (i - j) << 8;
-				goto out;
+	for (i = 0; i < 5; i++)
+		for (j = 0; j < 5; j++)
+			if (tmp == (data_high[i] << 4 | data_low[j])) {
+				bcm43xx_phy_write(bcm, 0x0069, (i - j) << 8 | 0x00C0);
+				return;
 			}
-		}
-	}
-out:
-	//XXX: Correct byte order of value?
-	tmp = *((u16 *)&value);
-	bcm43xx_phy_write(bcm, 0x0069, tmp);
 }
 
 int bcm43xx_radio_selectchannel(struct bcm43xx_private *bcm,
