a213d951c1330ec00fab54f96b5a37d310ad63e0
tuxbuesch <tuxbuesch@1c258df4-fefe-0310-a50c-9dade605666b>
2006-01-18 18:50:52 +0000

    add convenience script.

---
+++ trunk/sprom/sprommod.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	echo "Usage: $0 DEVICE [OPTIONS]"
+	echo ""
+	echo "DEVICE is the wireless device (eth0, wlan0, wlan0.11, ...)"
+	echo "OPTIONS are the options to bcm43xx_sprom"
+	exit 1
+fi
+
+device="$1"
+shift
+
+if [ -z "$device" ]; then
+	echo "No DEVICE given"
+	exit 1
+fi
+
+orig_data="$(iwpriv "$device" read_sprom)"
+err=$?
+if [ $err -ne 0 ]; then
+	echo "Could not read SPROM ($err)"
+	exit 1
+fi
+mod_data="$(echo "$orig_data" | bcm43xx_sprom $@)"
+err=$?
+if [ $err -ne 0 ]; then
+	echo "Could not modify SPROM data ($err)"
+	exit 1
+fi
+if [ -z "$mod_data" ]; then
+	echo "No data. Not modified?"
+	exit 1
+fi
+iwpriv "$device" write_sprom "$mod_data"
+err=$?
+if [ $err -ne 0 ]; then
+	echo "Could not write SPROM data ($err)"
+	exit 1
+fi
+
+exit 0
