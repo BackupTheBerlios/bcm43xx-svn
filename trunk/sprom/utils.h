f166f2013606eb530fb76ca8c2d5ca35f66b6653
tuxbuesch <tuxbuesch@1c258df4-fefe-0310-a50c-9dade605666b>
2006-02-12 12:52:47 +0000

    remove ugly ATTRIBUTE_PRINTF_1_2 and introduce ATTRIBUTE_FORMAT(f, a, b)

--- trunk/sprom/utils.h
+++ trunk/sprom/utils.h
@@ -5,18 +5,18 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#ifdef ATTRIBUTE_PRINTF_1_2
-# undef ATTRIBUTE_PRINTF_1_2
+#ifdef ATTRIBUTE_FORMAT
+# undef ATTRIBUTE_FORMAT
 #endif
 #ifdef __GNUC__
-# define ATTRIBUTE_PRINTF_1_2	__attribute__((format(printf, 1, 2)))
+# define ATTRIBUTE_FORMAT(t, a, b)	__attribute__((format(t, a, b)))
 #else
-# define ATTRIBUTE_PRINTF_1_2	/* nothing */
+# define ATTRIBUTE_FORMAT(t, a, b)	/* nothing */
 #endif
 
-int prinfo(const char *fmt, ...) ATTRIBUTE_PRINTF_1_2;
-int prerror(const char *fmt, ...) ATTRIBUTE_PRINTF_1_2;
-int prdata(const char *fmt, ...) ATTRIBUTE_PRINTF_1_2;
+int prinfo(const char *fmt, ...) ATTRIBUTE_FORMAT(printf, 1, 2);
+int prerror(const char *fmt, ...) ATTRIBUTE_FORMAT(printf, 1, 2);
+int prdata(const char *fmt, ...) ATTRIBUTE_FORMAT(printf, 1, 2);
 
 void * malloce(size_t size);
 void * realloce(void *ptr, size_t newsize);
